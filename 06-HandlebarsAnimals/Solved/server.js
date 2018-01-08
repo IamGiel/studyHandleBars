c92c98cf52e2509238c6b2adee2105f141dced75
Server <tilesmaster@tiless-mbp.attlocal.net>
2018-01-08 18:45:11 -0500

    separate array and export it - it worked.

--- 06-HandlebarsAnimals/Solved/server.js
+++ 06-HandlebarsAnimals/Solved/server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var list = require('./views/list.js');
 
 var app = express();
 var port = process.env.port || 3000;
@@ -8,49 +9,27 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var animals = [
-  {
-    animalType: "dog",
-    pet: true,
-    fierceness: 4
-  }, {
-    animalType: "cat",
-    pet: true,
-    fierceness: 10
-  }, {
-    animalType: "giraffe",
-    pet: false,
-    fierceness: 4
-  }, {
-    animalType: "zebra",
-    pet: false,
-    fierceness: 8
-  }, {
-    animalType: "lion",
-    pet: false,
-    fierceness: 10
-  }
-];
+
 
 app.get("/dog", function(req, res) {
   // Handlebars requires an object to be sent to the dog handlebars file.
-  // Lucky for us, animals[0] is an object!
+  // Lucky for us, list[0] is an object!
 
-  // 1. send the dog object from the animals array to the dog handlebars file.
-  res.render("dog", animals[0]);
+  // 1. send the dog object from the list array to the dog handlebars file.
+  res.render("dog", list[0]);
 });
 
 app.get("/all-pets", function(req, res) {
   // Handlebars requires an object to be sent to the index handlebars file.
 
-  // 2. Loop through the animals, and send those that are pets to the index handlebars file.
+  // 2. Loop through the list, and send those that are pets to the index handlebars file.
   var data = {
     anims: []
   };
 
-  for (var i = 0; i < animals.length; i += 1) {
+  for (var i = 0; i < list.length; i += 1) {
     // Get the current animal.
-    var currentAnimal = animals[i];
+    var currentAnimal = list[i];
 
     // Check if this animal is a pet.
     if (currentAnimal.pet) {
@@ -65,14 +44,14 @@ app.get("/all-pets", function(req, res) {
 app.get("/all-none-pets", function(req, res) {
   // Handlebars requires an object to be sent to the index handlebars file.
 
-  // 3. Loop through the animals, and send those that are not pets to the index handlebars file.
+  // 3. Loop through the list, and send those that are not pets to the index handlebars file.
   var data = {
     anims: []
   };
 
-  for (var i = 0; i < animals.length; i += 1) {
+  for (var i = 0; i < list.length; i += 1) {
     // Get the current animal.
-    var currentAnimal = animals[i];
+    var currentAnimal = list[i];
 
     // Check if this animal is a pet.
     if (!currentAnimal.pet) {
@@ -89,9 +68,9 @@ app.get("/fiercest", function(req, res) {
     anims: []
   };
 
-  for (var i = 0; i < animals.length; i += 1) {
+  for (var i = 0; i < list.length; i += 1) {
     // Get the current animal.
-    var currentAnimal = animals[i];
+    var currentAnimal = list[i];
     console.log(currentAnimal.fierceness);
     //Check if this animal is a pet.
     if (currentAnimal.fierceness == 10) {
---
+++ 06-HandlebarsAnimals/Solved/views/list.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    animalType: "dog",
+    pet: true,
+    fierceness: 4
+  }, {
+    animalType: "cat",
+    pet: true,
+    fierceness: 10
+  }, {
+    animalType: "giraffe",
+    pet: false,
+    fierceness: 4
+  }, {
+    animalType: "zebra",
+    pet: false,
+    fierceness: 8
+  }, {
+    animalType: "lion",
+    pet: false,
+    fierceness: 10
+  }
+];
